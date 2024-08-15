2f34fd83ee0bb3302d6e17603c0294143c07948e
Farhan Shahriyar <shahriyarfarhan3101@gmail.com>
2024-08-15 18:54:36 +0200

    prepare base model uploaded

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import sys
-from pathlib import Path
 
 # Add the 'src' directory to the Python path
-sys.path.append(str(Path(__file__).resolve().parent / "src"))
+sys.path.append('/home/farhan-shahriyar/Deep_Learning/Poultry_Shield-Deep-Learning-for-Poultry-Coccidiosis-Diagnosis/src')
 
-# Import the necessary modules
+# Now import the necessary modules
 from Poultry_Shield import logger
 from Poultry_Shield.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Poultry_Shield.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline        
+
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    data_ingestion = DataIngestionTrainingPipeline()
-    data_ingestion.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    logger.exception("An error occurred during the data ingestion stage")
-    raise e
+        logger.exception(e)
+        raise e
+
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
